e5922019fab3c838bfbae543aac9963ba924ed33
Shakhrizoda07 <shakhrizoda752@gmail.com>
2025-01-22 08:48:22 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+//let user = {
+  //  name: 'Shakhrizoda',
+  //  age: 17,
+  //  isAdmin: true,
+  // oytovola: ['husi', 'bonu', 'shoxruz']
+//};
+//console.log(user.name);
+//console.log(user.isAdmin);
+//console.log(user.oytovola);
+
+// const item = {
+//   name: 'Bonu'
+// };
+// item.name = 'Sasha';
+// console.log(item.name)
+
+// let user = {
+//     name: 'Shakhrizoda',
+//     age: 17,
+//     gender: 'women',
+//     school: 149,
+//     language: 'uzbek, russian',
+//     siblings: 'little sister',
+//     hobby: 'read book',
+//     bestie: 'Ziyoda',
+//     noutbook: 'hp',
+//     level: 'Java-Script',
+// };
+// let key = prompt('Что вы хотите узнать о пользователе?')
+// alert(user[key])
+
+let name = prompt('Ismingizni kiriting') 
+let id = prompt('Space idingizni kiriting')
+let password = prompt('Space passwordingizni kirirting')
+console.log(name)
+console.log(id)
+console.log(password)
